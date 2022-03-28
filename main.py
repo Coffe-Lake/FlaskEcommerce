4d61e6966f86fd219159d34baa2e526d2d0bd6c7
Coffe-Lake <felixkot00@gmail.com>
2022-03-29 01:57:57 +0300

    FlaskEcommerce project

--- main.py
+++ main.py
@@ -1,17 +1,68 @@
-from flask import Flask, render_template
-
+from crypt import methods
+from email.policy import default
+from flask import Flask, flash, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'Товар: {self.title}'
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/buy/<int:id>')
+def item_buy(id):
+    item = Item.query.get(id)
+
+    api = Api(metchant_id=12312312,
+              secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": 'RUB',
+        "amount": str(item.price) + '00'
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        title = data['title']
+        price = data['price']
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка добавления товара"
+    else:
+        return render_template('create.html')
 
 
 if __name__ == "__main__":
